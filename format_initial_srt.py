d78f4bc540c241bf8915c789ae98de6a1ddbf722
Joschka Rick <joschkarick@gmail.com>
2023-09-16 22:22:10 +0200

    Added UI

--- format_initial_srt.py
+++ format_initial_srt.py
@@ -1,27 +1,44 @@
-import tkinter as tk
+import PySimpleGUI as sg
 import srt
-from tkinter import filedialog
 
-root = tk.Tk()
-root.withdraw()
+layout = [
+    [sg.Text("Input SRT file:"), sg.In(), sg.FileBrowse(key="-BROWSE_INPUT")],
+    [sg.Text("Output SRT file:"), sg.In(), sg.FileSaveAs(key="-BROWSE_OUTPUT")],
+    [sg.Button("Format initial SRT file",key="-INITIAL_FORMAT"),sg.Button("Create script for voice-over")]
+]
 
-file_path = filedialog.askopenfilename()
-target_file_path = filedialog.asksaveasfilename()
+window = sg.Window("Subtitle Tools for The Week",layout)
 
-srt_file = open(file_path, 'r')
-target_file = open(target_file_path,'w')
-modified_srt_file = srt_file
-subs = list(srt.parse(srt_file))
+while True:
+    event, values = window.read()
 
-for idx, sub in enumerate(subs):
-    if sub.content.count('\n') == 0:
-        sub.content = sub.content + '\n'
-        print('Modifying line ', idx)
-    print(sub)
+    if event == "-INITIAL_FORMAT":
+        file_path = layout[0][1].get()
+        target_file_path = layout[1][1].get()
 
-target_file.write(srt.compose(subs))
+        srt_file = open(file_path, 'r')
+        target_file = open(target_file_path,'w')
+        modified_srt_file = srt_file
+        subs = list(srt.parse(srt_file))
 
-srt_file.close()
-target_file.close()
+        for idx, sub in enumerate(subs):
+            if sub.content.count('\n') == 0:
+                sub.content = sub.content + '\n'
+
+        target_file.write(srt.compose(subs))
+
+        srt_file.close()
+        target_file.close()
+
+        sg.popup_ok("Formatted SRT successfully saved",title="Success")
+
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+
 
 
